867a5dd2368a4e11e6e17947f7e30c3b0a130b64
Kole Lam <kolelamied1@gmail.com>
2023-05-22 17:45:04 -0600

    entered user prompts

--- index.js
+++ index.js
@@ -33,3 +33,41 @@ function writeToFile(fileName, answers) {
         err ? console.log(err) : console.log(`Nice! Logo created!`);
     });
 }
+
+function promptUser() {
+    inquirer.prompt([
+        {
+            type: `input`,
+            name: `text`,
+            message: `Enter text to be displayed on logo (up to 3 characters long)`,
+        },
+        {
+            type: `input`,
+            name: `textColor`,
+            message: `Select a color for the text`,
+        },
+        {
+            type: `list`,
+            name: `shape`,
+            message: `Select a shape for the logo`,
+            choices: [`square`, `triangle`, `circle`],
+        },
+        {
+            type: `input`,
+            name: `shapeColor`,
+            message: `Select a color for the shape`,
+        },
+    ])
+        .then((answers) => {
+            // error for text input as it needs to be 3 characters or less for logo as per the readme
+            if (answers.text.length > 3) {
+                console.log(`Error: Text must be 3 characters or less`);
+                return;
+            }
+            else {
+                writeToFile(`logo.svg`, answers);
+            }
+        });
+}
+// call function to prompt user for input
+promptUser();
